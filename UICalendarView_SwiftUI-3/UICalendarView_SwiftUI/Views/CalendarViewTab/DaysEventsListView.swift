5224901d7fbf60f78573f6b5af93b2e64153dee8
Eva Fiore <166539817+evafiore@users.noreply.github.com>
2024-05-06 16:15:06 -0400

    Add files via upload

---
+++ UICalendarView_SwiftUI-3/UICalendarView_SwiftUI/Views/CalendarViewTab/DaysEventsListView.swift
@@ -0,0 +1,59 @@
+//
+//  DaysEventsListView.swift
+//  UICalendarView_SwiftUI
+//
+//  Created by Eva Fiore on 5/6/24.
+//
+
+import SwiftUI
+
+struct DaysEventsListView: View {
+    @EnvironmentObject var eventStore: EventStore
+    @Binding var dateSelected: DateComponents?
+    @State private var formType: EventFormType?
+    var body: some View {
+        NavigationStack {
+            Group {
+                if let dateSelected {
+                    let foundEvents = eventStore.events.filter { $0.date.startOfDay == dateSelected.date!.startOfDay}
+                    List {
+                        ForEach(foundEvents) { events in
+                            ListViewRow(event: events, formType: $formType)
+                                .swipeActions {
+                                    Button(role: .destructive) {
+                                        eventStore.delete(events)
+                                    } label: {
+                                        Image(systemName: "trash")
+                                    }
+                                }
+                                .sheet(item: $formType) { $0 }
+                        }
+                    }
+                }
+            }
+            .navigationTitle(dateSelected?.date?.formatted(date: .long, time: .omitted) ?? "")
+        }
+    }
+}
+struct DaysEventsListView_Previews :PreviewProvider {
+    static var dateComponents: DateComponents {
+        var dateComponents = Calendar.current.dateComponents(
+            [.month,
+             .day,
+             .year,
+             .hour,
+             .minute], from: Date())
+        dateComponents.timeZone = TimeZone.current
+        dateComponents.calendar = Calendar(identifier: .gregorian)
+        return dateComponents
+    }
+    static var previews: some View {
+        DaysEventsListView(dateSelected: .constant(dateComponents))
+            .environmentObject(EventStore(preview: true)
+                              )
+    }
+}
+
+//#Preview {
+ //   DaysEventsListView()
+//}
